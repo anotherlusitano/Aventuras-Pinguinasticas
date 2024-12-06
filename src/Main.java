48e51bba27655859396f81793e1121bfbbe6ad96
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2024-12-06 23:41:30 +0000

    Remover o TODO na classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Ilha[] ilhas = null;
-        // TODO: Isto aqui passa a frente e nao esta a funcionar corretamente
         try {
             ilhas = Ilha.criarIlhas(9);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            //System.exit(1);
+            System.exit(1);
         }
 
         Jogador jogador = new Jogador("Antonio", -100, null);
